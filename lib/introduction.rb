5fcd5c27dec69f354e593681d26dc26af5a9652f
Ricky dildy <rdildy92@gmail.com>
2019-09-19 22:42:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,12 +2,12 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
-name = "Ricky"
-language = "Ruby"
+
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-
+name = "Ricky"
+#=> "Hi, my name is Ricky"
 def introduction_with_language(name, language)
   puts "Hi, name is #{name} and I am learning to program in #{language}." 
 end 
